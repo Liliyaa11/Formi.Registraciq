6469d1740698a792ce8ec9d3cdc1af522b7dd6e3
Liliyaa11 <120708561+Liliyaa11@users.noreply.github.com>
2023-10-23 12:35:13 +0300

    Add files via upload

---
+++ Vhod/Vhod/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vhod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string name = "Liliya";
+        string pas = "123456789";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text == name && textBox2.Text == pas)
+            {
+                MessageBox.Show("Вярно име!");
+                Form2 forma2 = new Form2();
+                Form1 forma1 = new Form1();
+              
+                forma2.Show();
+                forma1.Close();
+
+            }
+            else
+            { 
+
+                Form3 forma3 = new Form3();
+                forma3.Show();
+                MessageBox.Show("ГРЕШНО ИМЕ!");
+                
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Vhod/Vhod/Form3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Vhod
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+       
+
+        // buton za 4istene
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+
+
+        }
+        //registrirane
+        string name1 = "";
+        string pas1 = "";
+        string email = ""; 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //kakvo da izvede
+            if (textBox1.Text == name1 && textBox2.Text == pas1 && textBox3== pas1 && textBox4.Text==email)
+            {
+                MessageBox.Show("Вярно име!");
+                Form2 forma2 = new Form2();
+                Form1 forma1 = new Form1();
+
+                forma2.Show();
+                forma1.Close();
+
+            }
+            //
+            else
+            {
+
+                Form3 forma3 = new Form3();
+                forma3.Show();
+                MessageBox.Show("ГРЕШНО ИМЕ!");
+
+            }
+        }
+    }
+}
